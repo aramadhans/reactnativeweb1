f0fd4c521f1ecd38688c5f5b8d27f6c387e8d117
aramadhans <aramadhan@alamisharia.co.id>
2023-01-12 13:58:13 +0700

    first commit

---
+++ web/webpack.config.js
@@ -0,0 +1,110 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const appDirectory = path.resolve(__dirname, '../');
+
+// This is needed for webpack to compile JavaScript.
+// Many OSS React Native packages are not compiled to ES5 before being
+// published. If you depend on uncompiled packages they may cause webpack build
+// errors. To fix this webpack can be configured to compile to the necessary
+// `node_module`.
+const babelLoaderConfiguration = {
+  test: /\.(js)|(jsx)$/,
+  // Add every directory that needs to be compiled by Babel during the build.
+  include: [
+    path.resolve(appDirectory, 'index.web.js'),
+    path.resolve(appDirectory, 'src'),
+    path.resolve(appDirectory, 'node_modules/react-native-uncompiled'),
+  ],
+  use: {
+    loader: 'babel-loader',
+    options: {
+      cacheDirectory: true,
+      // The 'metro-react-native-babel-preset' preset is recommended to match React Native's packager
+      presets: ['module:metro-react-native-babel-preset'],
+      // Re-write paths to import only the modules needed by the app
+      plugins: [
+        'react-native-web',
+        [
+          'module-resolver',
+          {
+            alias: {
+              '^react-native$': 'react-native-web',
+            },
+          },
+        ],
+      ],
+    },
+  },
+};
+
+// This is needed for webpack to import static images in JavaScript files.
+const imageLoaderConfiguration = {
+  test: /\.(gif|jpe?g|png|svg)$/,
+  use: {
+    loader: 'url-loader',
+    options: {
+      name: '[name].[ext]',
+      esModule: false,
+    },
+  },
+};
+
+module.exports = {
+  entry: [
+    // load any web API polyfills
+    // path.resolve(appDirectory, 'polyfills-web.js'),
+    // your web-specific entry file
+    '@babel/polyfill',
+    path.resolve(appDirectory, 'index.web.js'),
+  ],
+
+  // configures where the build ends up
+  output: {
+    filename: 'bundle.web.js',
+    path: path.resolve(appDirectory, 'dist'),
+  },
+
+  // ...the rest of your config
+
+  module: {
+    rules: [
+      babelLoaderConfiguration,
+      imageLoaderConfiguration,
+      // {
+      //   test: /\.svg$/,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     {
+      //       loader: 'react-native-svg-web',
+      //     },
+      //   ],
+      // },
+    ],
+  },
+
+  resolve: {
+    // This will only alias the exact import "react-native"
+    alias: {
+      'react-native': 'react-native-web',
+    },
+    // If you're working on a multi-platform React Native app, web-specific
+    // module implementations should be written in files using the extension
+    // `.web.js`.
+    extensions: ['.web.js', '.js', '.web.jsx', '.jsx'],
+  },
+
+  plugins: [
+    // Add __DEV__ flag to browser example.
+    // new webpack.DefinePlugin({
+    //   __DEV__: process.env,
+    // }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development',
+      ),
+      // Add support for the __DEV__ global variable
+      __DEV__: process.env.NODE_ENV !== 'production',
+    }),
+  ],
+};
